e0572b0f93689a3e32cd477dd86830f30518507f
Vivian Huang <vivianhuang0130@gmail.com>
2017-09-23 13:27:19 -0700

    baseURL changed to heroku

--- src/auth.js
+++ src/auth.js
@@ -4,8 +4,8 @@ import jwtDecode from 'jwt-decode'
 class AuthClient {
   constructor() {
     this.request = axios.create({
-      baseURL: 'http://localhost:3001/api',
-      // https://evening-springs-26292.herokuapp.com/api
+      // baseURL: 'http://localhost:3001/api',
+      baseURL:'https://evening-springs-26292.herokuapp.com/api',
       headers: {
       // post:{
       //   token: //only for post token
