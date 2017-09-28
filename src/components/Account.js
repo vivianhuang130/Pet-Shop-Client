c271f12f1205bc518e1a2db858967dafdd23de1f
Yanny Castrillon <yannyandres117@gmail.com>
2017-09-28 00:02:54 -0700

    Refactor indentation regarding the Account component

--- src/components/Account.js
+++ src/components/Account.js
@@ -8,59 +8,58 @@ class Account extends React.Component{
     this.state = {products: [], orders: []}
   }
 
-componentDidMount(){
-  auth.getOrders()
-    .then((orders)=>{
-      console.log(orders)
-      this.setState({orders: orders.data})
-    })
-}
-
-handleEditSubmit(id,evt){
-  evt.preventDefault()
-  const formData = {
-    name: this.refs.name.value,
-    email: this.refs.email.value
+  componentDidMount(){
+    auth.getOrders()
+      .then((orders)=>{
+        console.log(orders)
+        this.setState({orders: orders.data})
+      })
   }
-  console.log("CREATING ACCOUNT...");
-  console.log(formData);
-  auth.editUser(formData,id).then((err)=>{
-    this.props.parent.setCurrentUser()
-  })
-}
-handleDeleteSubmit(id, evt){
-  evt.preventDefault()
-  console.log('deleting account')
-  auth.deleteUser(id)
-  .then((err) => {
-    this.props.parent.setCurrentUser()
-    this.setState({shouldRedirect: true})
-  })
-}
+
+  handleEditSubmit(id,evt){
+    evt.preventDefault()
+    const formData = {
+      name: this.refs.name.value,
+      email: this.refs.email.value
+    }
+    console.log("CREATING ACCOUNT...");
+    console.log(formData);
+    auth.editUser(formData,id).then((err)=>{
+      this.props.parent.setCurrentUser()
+    })
+  }
+  handleDeleteSubmit(id, evt){
+    evt.preventDefault()
+    console.log('deleting account')
+    auth.deleteUser(id)
+    .then((err) => {
+      this.props.parent.setCurrentUser()
+      this.setState({shouldRedirect: true})
+    })
+  }
 
   render(){
     return (
       <div>
-      <div className="user-info">
+        <div className="user-info">
           <h1>Edit User Info <span role='img' aria-label="Emoji blond">🙋</span></h1>
           <p>Name: {this.props.currentUser.name}</p>
           <br/>
           <p>Email: {this.props.currentUser.email}</p>
           <h2>{this.props.currentUser.password}</h2>
           {console.log(this.props.currentUser)}
-
-      </div>
-      <div className="update">
-        <form onSubmit={this.handleEditSubmit.bind(this, this.props.currentUser._id)}>
+        </div>
+        <div className="update">
+          <form onSubmit={this.handleEditSubmit.bind(this, this.props.currentUser._id)}>
             Update name: <input className="form" ref='name' type="text" defaultValue={this.props.currentUser.name} />
             <br />
             Update email: <input className="form" ref='email' type="text" defaultValue={this.props.currentUser.email} />
-          <div>
-            <button className="update-delete-account-button">Update Account</button>
-            <button className="update-delete-account-button" onClick ={this.handleDeleteSubmit.bind(this, this.props.currentUser._id)}>Delete Account</button>
-          </div>
-        </form>
-      </div>
+            <div>
+              <button className="update-delete-account-button">Update Account</button>
+              <button className="update-delete-account-button" onClick ={this.handleDeleteSubmit.bind(this, this.props.currentUser._id)}>Delete Account</button>
+            </div>
+          </form>
+        </div>
         <div className="order-history">
           <h1>Order History</h1>
             {this.state.orders.reverse().map((o) => (
@@ -68,23 +67,17 @@ handleDeleteSubmit(id, evt){
                 Created At: {o.createdAt}
                 <br/>
                 Quantity: {o.products.length}
-                  {o.products.map((p, i)=>
-                    (
-                      <li key={i._id}>
-                        <p>Name: { p.name } </p> <br/>
-                        <p>Price: ${p.price}</p>
-
-                      </li>
-                    ))}
+                  {o.products.map((p, i)=> (
+                    <li key={i._id}>
+                      <p>Name: { p.name } </p> <br/>
+                      <p>Price: ${p.price}</p>
+                    </li>
+                  ))}
               </div>
-
-              ))
-            }
-
+            ))}
         </div>
-
       </div>
-      )
+    )
   }
 }
 
